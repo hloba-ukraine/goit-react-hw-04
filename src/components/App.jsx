8931839021b05cfb20a7427710531bbd31ac7f8a
hloba-ukraine <138844087+hloba-ukraine@users.noreply.github.com>
2024-04-06 00:57:29 -0600

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
         );
 
         const img = data.results;
-        setImages([...images, ...img]);
+        setImages((prevImages) => [...prevImages, ...img]);
 
         console.log(images);
       } catch (error) {
